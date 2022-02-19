3b6363387259954721a69c8da9f785e7ce3912b2
CuteAmebka <MrDimarix@gmail.com>
2022-02-19 15:27:24 +0300

    Сделано второе задание(очередь)

---
+++ QueueTask/QueueTask/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QueueTask
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        MyQueue<int> Numbers = new MyQueue<int>();
+        private void fillListBox()
+        {
+            listBox1.Items.Clear();
+
+            for (int i = 0; i < Numbers.MyArray().Length; i++)
+            {
+                listBox1.Items.Add(Numbers.MyArray()[i].ToString());
+            }
+
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EnqueueBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Numbers.enQueue(Convert.ToInt32(elementTxt.Text));
+                fillListBox();
+            }
+            catch
+            {
+                MessageBox.Show("Возникла ошибка");
+            }
+        }
+
+        private void DequeueBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Извлечен элемент: " + Numbers.deQueue());
+                fillListBox();
+            }
+            catch
+            {
+                MessageBox.Show("Возникла ошибка");
+            }
+        }
+
+        private void PeekBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MessageBox.Show("Ваш элемент: " + Numbers.Peek());
+                fillListBox();
+            }
+            catch
+            {
+                MessageBox.Show("Возникла ошибка");
+            }
+        }
+    }
+}
---
+++ QueueTask/QueueTask/MyQueue.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueTask
+{
+    class MyQueue<T>
+    {
+        private int SIZE = 5; 
+        private int currentOut, currentIn;
+        private T[] items;
+
+        public MyQueue()
+        {
+            currentOut = -1;
+            currentIn = -1;
+            items = new T[SIZE];
+        }
+
+        public T[] MyArray()
+        {
+            return items;
+        }
+        bool isFull()
+        {
+            if (currentOut == 0 && currentIn == SIZE - 1)
+            {
+                return true;
+            }
+            if (currentOut == currentIn + 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        bool isEmpty()
+        {
+            if (currentOut == -1)
+                return true;
+            else
+                return false;
+        }
+
+
+        public void enQueue(T element)
+        {
+            if (isFull())
+            {
+                throw new Exception("Очередь заполнена");
+            }
+            else
+            {
+                if (currentOut == -1)
+                    currentOut = 0;
+                currentIn = (currentIn + 1) % SIZE;
+                items[currentIn] = element;
+            }
+        }
+
+        // Удаление элемента
+        public T deQueue()
+        {
+            T element;
+            if (isEmpty())
+            {
+                throw new Exception("Очередь пуста");
+            }
+            else
+            {
+                element = items[currentOut];
+                if (currentOut == currentIn)
+                {
+                    items[currentOut] = default(T);
+                    currentOut = -1;
+                    currentIn = -1;
+                } 
+                else
+                {
+                    items[currentOut] = default(T);
+                    currentOut = (currentOut + 1) % SIZE;
+                }
+                return (element);
+            }
+        }
+        public T Peek()
+        {
+            if (isEmpty())
+            {
+                throw new Exception("Очередь пуста");
+            }
+            else
+            {
+                return (items[currentOut]);
+            }
+        }
+    }
+}
