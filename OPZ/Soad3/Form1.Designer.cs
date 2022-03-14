1508eed3c3a4845f6852ad3a8ee3815fc2c384d4
CuteAmebka <MrDimarix@gmail.com>
2022-03-14 21:01:20 +0300

    Добавлено 4 задание

---
+++ OPZ/Soad3/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Soad3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.Button();
+            this.calculate = new System.Windows.Forms.Button();
+            this.input_text = new System.Windows.Forms.TextBox();
+            this.OPZ_text = new System.Windows.Forms.TextBox();
+            this.calculated_value = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(416, 73);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(75, 23);
+            this.input.TabIndex = 0;
+            this.input.Text = "input";
+            this.input.UseVisualStyleBackColor = true;
+            this.input.Click += new System.EventHandler(this.input_Click);
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(416, 154);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 1;
+            this.calculate.Text = "calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // input_text
+            // 
+            this.input_text.Location = new System.Drawing.Point(97, 38);
+            this.input_text.Name = "input_text";
+            this.input_text.Size = new System.Drawing.Size(246, 20);
+            this.input_text.TabIndex = 2;
+            // 
+            // OPZ_text
+            // 
+            this.OPZ_text.Location = new System.Drawing.Point(97, 138);
+            this.OPZ_text.Name = "OPZ_text";
+            this.OPZ_text.Size = new System.Drawing.Size(246, 20);
+            this.OPZ_text.TabIndex = 3;
+            // 
+            // calculated_value
+            // 
+            this.calculated_value.Location = new System.Drawing.Point(97, 238);
+            this.calculated_value.Name = "calculated_value";
+            this.calculated_value.Size = new System.Drawing.Size(246, 20);
+            this.calculated_value.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Normal format";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "OPZ format";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(94, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "calculated value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.calculated_value);
+            this.Controls.Add(this.OPZ_text);
+            this.Controls.Add(this.input_text);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button input;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.TextBox input_text;
+        private System.Windows.Forms.TextBox OPZ_text;
+        private System.Windows.Forms.TextBox calculated_value;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ OPZ/Soad3/OPZ.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soad3
+{
+    class OPZ
+    {
+        static private bool IsDelimeter(char c)
+        {
+            if ((" ".IndexOf(c) != -1))
+                return true;
+            return false;
+        }
+
+        static private bool IsOperator(char с)
+        {
+            if (("+-/*^()".IndexOf(с) != -1))
+                return true;
+            return false;
+        }
+
+        static private byte GetPriority(char s)
+        {
+            switch (s)
+            {
+                case '(': return 0;
+                case ')': return 1;
+                case '+': return 2;
+                case '-': return 3;
+                case '*': return 4;
+                case '/': return 4;
+                case '^': return 5;
+                default: return 6;
+            }
+        }
+
+        static public string GetExpression(string input)
+        {
+            string output = string.Empty;
+            Stack<char> Stack = new Stack<char>(); 
+
+            for (int i = 0; i < input.Length; i++) 
+            {
+                
+                if (IsDelimeter(input[i]))
+                    continue; 
+
+                
+                if (Char.IsDigit(input[i])) //Записываем число
+                {
+                    
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i]))
+                    {
+                        output += input[i]; 
+                        i++; 
+
+                        if (i == input.Length) break; 
+                    }
+
+                    output += " "; 
+                    i--;
+                    continue;
+                }
+
+                
+                if (IsOperator(input[i])) 
+                {
+                    if (input[i] == '(')  { Stack.Push(input[i]); continue; }
+                    if (input[i] == ')')
+                    {
+                        char s = Stack.Pop();
+                        while (s != '(')
+                        {
+                            output += s.ToString() + ' ';
+                            s = Stack.Pop();
+                        }
+                        continue;
+                    }
+                    
+                    {
+                        if (Stack.Count > 0)
+                            if (GetPriority(input[i]) <= GetPriority(Stack.Peek())) //И если приоритет нашего оператора меньше или равен приоритету оператора на вершине стека
+                                output += Stack.Pop().ToString() + " "; //То добавляем последний оператор из стека в строку с выражением
+
+                        Stack.Push(char.Parse(input[i].ToString())); //Если стек пуст, или же приоритет оператора выше - добавляем операторов на вершину стека
+
+                    }
+                }
+            }
+
+            
+            while (Stack.Count > 0)
+                output += Stack.Pop() + " ";
+
+            return output; 
+        }
+
+        static public double Counting(string input)
+        {
+            double result = 0;
+            Stack<double> temp = new Stack<double>(); 
+
+            for (int i = 0; i < input.Length; i++) 
+            {
+                
+                if (Char.IsDigit(input[i]))
+                {
+                    string a = string.Empty;
+
+                    while (!IsDelimeter(input[i]) && !IsOperator(input[i])) //Пока не разделитель,записываем все число
+                    {
+                        a += input[i]; 
+                        i++;
+                        if (i == input.Length) break;
+                    }
+                    temp.Push(double.Parse(a)); //Записываем в стек
+                    i--;
+                }
+                else if (IsOperator(input[i])) //Если символ - оператор
+                {
+                    
+                    double a = temp.Pop();
+                    double b = temp.Pop();
+
+                    switch (input[i]) //Делаем наше действие
+                    {
+                        case '+': result = b + a; break;
+                        case '-': result = b - a; break;
+                        case '*': result = b * a; break;
+                        case '/': result = b / a; break;
+                        case '^': result = double.Parse(Math.Pow(double.Parse(b.ToString()), double.Parse(a.ToString())).ToString()); break;
+                    }
+                    temp.Push(result); //Записываем результат вычисления
+                }
+            }
+            return temp.Peek(); //Берем конечный ответ (последнее в стеке)
+        }
+
+        static public double Calculate(string input)
+        {
+            string output = GetExpression(input); 
+            double result = Counting(output); 
+            return result;
+        }
+    }
+}
